35f2a1df68a88854e82a9b493d7ec9d5aed90386
mark47B <windachae47@gmail.com>
2023-05-13 13:17:05 +0300

    Create class Edge and add pydantic

--- python-graphs/base.py
+++ python-graphs/base.py
@@ -1,6 +1,13 @@
 from typing import Optional
+from pydantic import BaseModel
 
 
-class Node:
+class Node(BaseModel):
     number: int
     node_degree: Optional[int]
+
+
+class Edge(BaseModel):
+    start_node: Node
+    end_node: Node
+    weight: int
