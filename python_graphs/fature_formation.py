5e80a032ebec812e6a04115d5bf4cb471a608d8b
mark47B <windachae47@gmail.com>
2023-05-25 06:13:56 +0300

    Move funcs to structure

--- python_graphs/fature_formation.py
+++ python_graphs/fature_formation.py
@@ -12,20 +12,27 @@
 import numpy as np
 import pandas as pd
 
+
 def node_degree(numbers_of_nodes:np.array, adjacency_matrix:np.array):
     return np.sum(adjacency_matrix[numbers_of_nodes,:],axis=1)
+
 def common_neighbours(u:int, v:int, adjacency_matrix:np.array) -> int:
     return np.sum(np.where(adjacency_matrix[u,:]+adjacency_matrix[v,:] != 0, 1, 0))
+
 def adamic_adar(u:int, v:int, adjacency_matrix:np.array) -> float:
     return np.sum(1/node_degree(np.nonzero(adjacency_matrix[u,:]+adjacency_matrix[v,:]),adjacency_matrix))
+
 def jaccard_coefficient(u:int, v:int, adjacency_matrix:np.array) -> float:
     return np.sum(np.where(adjacency_matrix[u,:]-adjacency_matrix[v,:] == 0, 1, 0))/np.sum(
         np.where(adjacency_matrix[u,:]+adjacency_matrix[v,:] != 0, 1, 0))
+
 def preferential_attachment(u:int, v:int, adjacency_matrix:np.array) -> int:
     return np.prod(node_degree(np.array([u,v]),adjacency_matrix))
 
 # 3 функции для вычисления весов
+
 # Во все функции в качестве t можно передавать np массив временных меток всех ребер
+
 def temporal_weighting_w_linear(t:np.array,t_min:int,t_max:int,l:float=0.2) -> float:
     return l+(1-l)*(t-t_min)/(t_max-t_min) # w_linear
 def temporal_weighting_w_exponential(t:np.array,t_min:float,t_max:float,l:float=0.2) -> float:
@@ -34,7 +41,8 @@ def temporal_weighting_w_square_root(t:np.array,t_min:float,t_max:float,l:float=
     return l+(1-l)*np.sqrt((t-t_min)/(t_max-t_min)) #w_square_root
 
 # 7 функций для аггрегации весов ребер, прилегающих к узлу
-# edge_weights - np array размерности 2; i-ая строка - веса ребер, прилегающих к i-ой вершине
+# edge_weights - np array размерности 2; i-ая строка - веса ребер, прилегающих к i-ой вершине 
+
 def aggregation_of_node_activity_zeroth_quantile(edges_weights:list)-> float:
     return [np.quantile(weights_set,0) for weights_set in edges_weights]
 def aggregation_of_node_activity_first_quantile(edges_weights:list)-> float:
@@ -51,6 +59,7 @@ def aggregation_of_node_activity_mean(edges_weights:list)-> float:
     return [np.mean(weights_set,0) for weights_set in edges_weights]
     
 # 4 функции для объединения статистик по парам узлов
+
 def combining_node_activity_sum(aggregarion_node_one:np.array, aggregation_node_two:np.array)-> float:
     return aggregarion_node_one + aggregation_node_two
 def combining_node_activity_absolute_diference(aggregarion_node_one:np.array, aggregation_node_two:np.array)-> float:
@@ -73,18 +82,23 @@ def replace_nan(df, columns, start_node_column, end_node_column):
     Замена NaN на [] в числовых ячейках и сопоставление номеров для вершин,
     которые были только либо в end_node, либо в start_node
     '''
+    
     for column in columns:
         df[column] = df[column].apply(lambda x: [] if not isinstance(x, list) else x)
-    df[start_node_column] = np.where(not isinstance(df[start_node_column], int), df[end_node_column], df[start_node_column])
-    df[end_node_column] = np.where(not isinstance(df[end_node_column], int), df[start_node_column], df[end_node_column])
+    
+    df[start_node_column] = np.where(np.isnan(df[start_node_column]), df[end_node_column], df[start_node_column])
+
     return df
 
+
 def aggregation_of_node_activity(node: pd.DataFrame, edges_weights_for_node: pd.DataFrame):
+    
     '''
     Агрегация активности узлов на основе 7 функций: 
     нулевой, первый,второй,третий,чертвертый квантили; сумма и среднее
     по весам ребер смежных с вершиной
     '''
+    
     node['node_activity_zeroth_quantile_wl'] = aggregation_of_node_activity_zeroth_quantile(edges_weights_for_node["weight_linear"])
     node['node_activity_first_quantile_wl'] = aggregation_of_node_activity_first_quantile(edges_weights_for_node["weight_linear"])
     node['node_activity_second_quantile_wl'] = aggregation_of_node_activity_second_quantile(edges_weights_for_node["weight_linear"])
@@ -108,13 +122,17 @@ def aggregation_of_node_activity(node: pd.DataFrame, edges_weights_for_node: pd.
     node['node_activity_fourth_quantile_wsr'] = aggregation_of_node_activity_fourth_quantile(edges_weights_for_node["weight_square_root"])
     node['node_activity_sum_wsr'] = aggregation_of_node_activity_sum(edges_weights_for_node["weight_square_root"])
     node['node_activity_mean_wsr'] = aggregation_of_node_activity_mean(edges_weights_for_node["weight_square_root"])
+    
+
 
 def combining_node_activity(node: pd.DataFrame)->pd.DataFrame:
+    
     '''
     Объединение активности узлов для формирования векторного описания
     ребра на основе 4 функций:
     сумма, абсолютная разность, мин, макс 
     по парным активностям инцидентных вершин
+    
     '''
     values = node[['node_activity_zeroth_quantile_wl',
     'node_activity_first_quantile_wl',
@@ -143,6 +161,7 @@ def combining_node_activity(node: pd.DataFrame)->pd.DataFrame:
     num_of_nodes = node.shape[0]
     num_of_feature = 21
 
+    
     combined_values = []
     start_nodes_vector = []
 
@@ -152,6 +171,7 @@ def combining_node_activity(node: pd.DataFrame)->pd.DataFrame:
         
         combined_values.extend(repeated_row)
         start_nodes_vector.extend(repeated_node)
+
 
     # Преобразование объединенных значений в массив NumPy
     first_combined_array = np.array(combined_values)
@@ -177,30 +197,32 @@ def combining_node_activity(node: pd.DataFrame)->pd.DataFrame:
     Edge_feature = pd.DataFrame({"start_node":start_nodes_vector, "end_node":end_nodes_vector, feature_column_name: all_feature})
     return (Edge_feature,feature_column_name)
 
+
 def make_edges_weights_adjacent_to_node(edge: pd.DataFrame):
+    
     '''
     Формирование датафрейма с весами примыкающих к вершине ребер.
     Строка соответствует определенной вершине.
     '''
-    grouped_by_start_node = edge.drop(['end_node',"number","last_timestamp"],
+    grouped_by_start_node = edge.drop(['end_node',"number","last_timestamp"], 
                                       axis=1).groupby("start_node").agg(
     {'weight_linear': list, 'weight_exponential': list,
      'weight_square_root': list}).reset_index()
-    grouped_by_end_node = edge.drop(['start_node',"number","last_timestamp"],
+    grouped_by_end_node = edge.drop(['start_node',"number","last_timestamp"], 
                                     axis=1).groupby("end_node").agg(
     {'weight_linear': list, 'weight_exponential': list,
      'weight_square_root': list}).reset_index()
-
+    
     edges_weights_for_node = grouped_by_start_node.merge(
         grouped_by_end_node, left_on='start_node', right_on='end_node', how='outer')
 
 
     edges_weights_for_node = replace_nan(edges_weights_for_node,
                                          ['weight_linear_x', 'weight_exponential_x',
-                                          "weight_square_root_x","weight_linear_y",
+                                          "weight_square_root_x","weight_linear_y", 
                                           'weight_exponential_y', "weight_square_root_y"],
                                          'start_node',"end_node")
-
+    
     edges_weights_for_node["weight_linear"]=edges_weights_for_node[
         "weight_linear_x"] + edges_weights_for_node["weight_linear_y"]
     edges_weights_for_node["weight_exponential"]=edges_weights_for_node[
@@ -209,16 +231,17 @@ def make_edges_weights_adjacent_to_node(edge: pd.DataFrame):
         "weight_square_root_x"]+edges_weights_for_node["weight_square_root_y"]
     edges_weights_for_node = edges_weights_for_node.drop(
         ["end_node",'weight_linear_x', 'weight_exponential_x',
-         "weight_square_root_x","weight_linear_y", 'weight_exponential_y',
+         "weight_square_root_x","weight_linear_y", 'weight_exponential_y', 
          "weight_square_root_y"], axis=1)
 
     edges_weights_for_node['start_node'] = edges_weights_for_node['start_node'].astype(int)
+
     edges_weights_for_node[[
         "weight_linear","weight_exponential","weight_square_root"]] = edges_weights_for_node[[
         "weight_linear","weight_exponential","weight_square_root"]].apply(lambda x: np.array(x))
 
     edges_weights_for_node=edges_weights_for_node.sort_values(by='start_node')
-
+    
     return edges_weights_for_node
 
 def split_list_cell(df: pd.DataFrame, column_name:str):
@@ -254,6 +277,7 @@ def count_static_topological_features(df: pd.DataFrame):
     df["jaccard_coefficient"] = df.apply(lambda row: jaccard_coefficient(row["start_node"],row["end_node"],adjacency_matrix), axis=1)
     df["preferential_attachment"] = df.apply(lambda row: preferential_attachment(row["start_node"],row["end_node"],adjacency_matrix), axis=1)
 
+
 def feature_for_edges(edge: pd.DataFrame, node: pd.DataFrame, adjacency_matrix: np.array, t_min:int, t_max:int):
     '''
     Получение датафрейма с признаками для ребер
@@ -273,11 +297,154 @@ def feature_for_edges(edge: pd.DataFrame, node: pd.DataFrame, adjacency_matrix: 
     Edge_feature = split_list_cell(Edge_feature, feature_column_name)
 
     return Edge_feature
-
 # Example:
 #       Edge_feature = feature_for_edges(edge, node, adjacency_matrix, 0, 20000000000)
 
+def combining_node_activity_for_absent_edge(node: pd.DataFrame, edge: pd.DataFrame)->pd.DataFrame:
+    
+    '''
+    Объединение активности узлов для формирования векторного описания
+    ребра на основе 4 функций:
+    сумма, абсолютная разность, мин, макс 
+    по парным активностям инцидентных вершин
+    
+    '''
+    values = node[['node_activity_zeroth_quantile_wl',
+    'node_activity_first_quantile_wl',
+    'node_activity_second_quantile_wl',
+    'node_activity_third_quantile_wl',
+    'node_activity_fourth_quantile_wl',
+    'node_activity_sum_wl',
+    'node_activity_mean_wl',
+    
+    'node_activity_zeroth_quantile_we',
+    'node_activity_first_quantile_we',
+    'node_activity_second_quantile_we',
+    'node_activity_third_quantile_we',
+    'node_activity_fourth_quantile_we',
+    'node_activity_sum_we',
+    'node_activity_mean_we',
+    
+    'node_activity_zeroth_quantile_wsr',
+    'node_activity_first_quantile_wsr',
+    'node_activity_second_quantile_wsr',
+    'node_activity_third_quantile_wsr',
+    'node_activity_fourth_quantile_wsr',
+    'node_activity_sum_wsr',
+    'node_activity_mean_wsr']].values
+
+    num_of_nodes = node.shape[0]
+    num_of_feature = 21
+    feature_column_name = "feature_vector"
 
 
+    combined_values = []
+    start_nodes_vector = []
 
+    for i, row in enumerate(values):
+        repeated_node = np.tile(i, num_of_nodes - i - 1)
+        start_nodes_vector.extend(repeated_node)
+        
+    end_nodes_vector = list(np.concatenate([np.arange(i, num_of_nodes) for i in range(1, num_of_nodes)]))
+    
+    Edge_feature = pd.DataFrame({"start_node":start_nodes_vector, "end_node":end_nodes_vector})
 
+    
+    # Удаление строк из первого датафрейма, где значения в столбцах 'a' и 'b' совпадают со значениями во втором датафрейме
+    df_merged = Edge_feature.merge(edge[['start_node', 'end_node']], on=['start_node', 'end_node'], how='left', indicator=True)
+    df_filtered = df_merged[df_merged['_merge'] == 'left_only']
+    df_filtered = df_filtered.drop(columns='_merge')
+    Edge_feature = df_filtered
+    
+    start_nodes_vector = Edge_feature['start_node'].values
+    end_nodes_vector = Edge_feature['end_node'].values
+
+    
+    first_combined_array = np.concatenate([values[start_nodes_vector[i]] for i in range(len(start_nodes_vector))])
+    
+    second_combined_array = np.concatenate([values[end_nodes_vector[i]] for i in range(len(end_nodes_vector))])
+    
+    feature_by_sym = combining_node_activity_sum(first_combined_array,second_combined_array)
+    feature_by_abs_dif = combining_node_activity_absolute_diference(first_combined_array,second_combined_array)
+    feature_by_min = combining_node_activity_minimum(first_combined_array,second_combined_array)
+    feature_by_max = combining_node_activity_maximum(first_combined_array,second_combined_array)
+
+    
+    all_feature = [np.concatenate([feature_by_sym[i:i+num_of_feature], 
+                                   feature_by_abs_dif[i:i+num_of_feature], 
+                                   feature_by_min[i:i+num_of_feature], 
+                                   feature_by_max[i:i+num_of_feature]]) 
+                   for i in range(0, feature_by_sym.shape[0], num_of_feature)]
+    
+    Edge_feature[feature_column_name] =  all_feature
+    return (Edge_feature,feature_column_name)
+
+def feature_for_absent_edges(edge: pd.DataFrame, node: pd.DataFrame, adjacency_matrix: np.array, t_min:int, t_max:int):
+    '''
+    Получение датафрейма с признаками для ребер
+    '''
+    temporal_weighting(edge, t_min, t_max)
+    
+    edges_weights_adjacent_to_node = make_edges_weights_adjacent_to_node(edge)
+    
+    aggregation_of_node_activity(node,edges_weights_adjacent_to_node)
+    
+    Edge_feature,feature_column_name = combining_node_activity_for_absent_edge(node,edge)
+
+    Edge_feature = merge_with_temporal_graph_number(Edge_feature, node)
+    
+    count_static_topological_features(Edge_feature)
+            
+    Edge_feature = split_list_cell(Edge_feature, feature_column_name)
+
+    return Edge_feature
+
+# Создаем датафрейм с помощью конструктора pd.DataFrame()
+edge = pd.DataFrame({
+    'start_node': [0, 0],
+    'end_node': [1, 2],
+    'number': [100, 200],
+    'last_timestamp': [1634558000, 1634558100],
+    'weight_linear': [None, None],
+    'weight_exponential': [None, None],
+    'weight_square_root': [None, None]
+})
+
+# Создаем датафрейм с помощью конструктора pd.DataFrame()
+node = pd.DataFrame({
+    'number': [0, 1, 2],
+    'number_in_temporal_graph': [100, 31, 42],
+    'node_degree': [2, 3, 1],
+    'node_activity_zeroth_quantile_wl': [None for i in range(3)],
+    'node_activity_first_quantile_wl': [None for i in range(3)],
+    'node_activity_second_quantile_wl': [None for i in range(3)],
+    'node_activity_third_quantile_wl': [None for i in range(3)],
+    'node_activity_fourth_quantile_wl': [None for i in range(3)],
+    'node_activity_sum_wl': [None for i in range(3)],
+    'node_activity_mean_wl': [None for i in range(3)],
+    
+    'node_activity_zeroth_quantile_we': [None for i in range(3)],
+    'node_activity_first_quantile_we': [None for i in range(3)],
+    'node_activity_second_quantile_we': [None for i in range(3)],
+    'node_activity_third_quantile_we': [None for i in range(3)],
+    'node_activity_fourth_quantile_we': [None for i in range(3)],
+    'node_activity_sum_we': [None for i in range(3)],
+    'node_activity_mean_we': [None for i in range(3)],
+    
+    'node_activity_zeroth_quantile_wsr': [None for i in range(3)],
+    'node_activity_first_quantile_wsr': [None for i in range(3)],
+    'node_activity_second_quantile_wsr': [None for i in range(3)],
+    'node_activity_third_quantile_wsr': [None for i in range(3)],
+    'node_activity_fourth_quantile_wsr': [None for i in range(3)],
+    'node_activity_sum_wsr': [None for i in range(3)],
+    'node_activity_mean_wsr': [None for i in range(3)],
+    
+})
+
+adjacency_matrix = np.array([[1,1,1],[1,0,1],[1,1,1]])
+
+Edge_feature = feature_for_edges(edge, node, adjacency_matrix, 0, 20000000000)
+
+Edge_feature = feature_for_absent_edges(edge, node, adjacency_matrix, 0, 20000000000)
+
+    
---
+++ python_graphs/model-training.py
@@ -0,0 +1,62 @@
+def train_test_split_temporal_graph(edge_list:list, split_ratio: float):
+    '''
+    Разделение выборки на части формирования признаков и на части предсказания
+    '''
+    edge_list_feature_build_part = edge_list[:int(len(edge_list)*split_ratio)]
+    edge_list_prediction_part = edge_list[len(edge_list_feature_build_part):]
+    return (edge_list_feature_build_part,edge_list_prediction_part)
+
+
+
+
+def get_performance(edge_list:list, split_ratio: float, t_min:int, t_max:int):
+    
+    edge_list_feature_build_part, edge_list_prediction_part = train_test_split_temporal_graph(edge_list:list, split_ratio: float)
+    
+    'формирование статик графа и получение для него edge,node,adjacency_matrix'
+    
+    Edge_feature = feature_for_edges(edge_feature_build_part, node_feature_build_part, adjacency_matrix_feature_build_part, t_min, t_max)
+    
+    # выделить в отдельную функцию по присоединению 
+    # номеров вершин из темпорального графа к edge_list
+    edge_prediction_part = edge_prediction_part.merge(
+        node_prediction_part['number','number_in_temporal_graph'], left_on='start_node', right_on='number', how='left')
+    
+    edge_prediction_part = edge_prediction_part.rename(columns={'number_in_temporal_graph': 'number_in_temporal_graph_start_node'})
+
+    
+    edge_prediction_part = edge_prediction_part.merge(
+        node_prediction_part['number','number_in_temporal_graph'], left_on='end_node', right_on='number', how='left')
+    
+    edge_prediction_part = edge_prediction_part.rename(columns={'number_in_temporal_graph': 'number_in_temporal_graph_end_node'})
+
+    
+    
+    
+    Edge_feature = Edge_feature.merge(
+        edge_prediction_part['number_in_temporal_graph_start_node','number_in_temporal_graph_end_node','number'], 
+        left_on=['number_in_temporal_graph_start_node','number_in_temporal_graph_end_node'], 
+        right_on=['number_in_temporal_graph_start_node','number_in_temporal_graph_end_node'], 
+        how='left')
+
+    
+    Edge_feature['number'] = Edge_feature['number'].apply(lambda x: 0 if np.isnan(x) else 0)
+
+    X = Edge_feature.drop(['number'])
+    
+    y = Edge_feature['number']
+    
+    X_train, X_test, y_train, y_test = (
+        sklearn.model_selection.train_test_split(X, y, random_state=random_state))
+    
+    pipe = sklearn.pipeline.make_pipeline(
+            sklearn.preprocessing.StandardScaler(),
+            sklearn.linear_model.LogisticRegression(max_iter=10000, n_jobs=-1, 
+                                                    random_state=42)
+        )
+    pipe.fit(X_train, y_train)
+    
+    auc = sklearn.metrics.roc_auc_score(
+        y_true=y_test, y_score=pipe.predict_proba(X_test)[:,1])
+    
+    return auc
